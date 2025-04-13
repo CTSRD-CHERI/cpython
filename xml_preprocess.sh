b9d2f5269c7c39852d49d62a32162646e025252f
qianhui wang <qw304@cam.ac.uk>
2025-04-13 22:59:13 +0100

    use perl instead sed to match patterns on a single line

--- xml_preprocess.sh
+++ xml_preprocess.sh
@@ -3,9 +3,7 @@
 # Find all regret*.xml files, skipping permission denied errors
 find ./ -name "regret*.xml" 2>&1 | grep -v "Permission denied" | while read -r file; do
   echo "Processing $file"
-  sed -i '' '/<testcase /{
-  	/classname=/!s/<testcase /<testcase classname="" /
-  }' "$file"
-
+  # perl -pi -e 's/classname="NoClassName"/classname=""/g' "$file"
+  perl -pi -e 's{<testcase(?!\s+classname=)}{<testcase classname="NoClassName"}g' "$file"
 done
 

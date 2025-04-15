ba8ccea061774d0dbc1823ebd1ae0a91ae4b09b4
qianhui wang <qw304@cam.ac.uk>
2025-04-15 16:25:36 +0100

    restore buffer files

--- xml_preprocess.sh
+++ xml_preprocess.sh
@@ -4,6 +4,6 @@
 find ./ -name "regret*.xml" 2>&1 | grep -v "Permission denied" | while read -r file; do
   echo "Processing $file"
   # perl -pi -e 's/classname="NoClassName"/classname=""/g' "$file"
-  perl -pi -e 's{<testcase(?!\s+classname=)}{<testcase classname="NoClassName"}g' "$file"
+  perl -pi -e 's{<testcase(?!\s+classname=)}{<testcase classname=""}g' "$file"
 done
 
